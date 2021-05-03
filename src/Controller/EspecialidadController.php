1519878a48b786e9f744a2279a5a9a5904fbc278
Michael Rodriguez Alvarez <michaelrodriguezalvarez@gmail.com>
2021-05-04 00:17:20 +0200

    Creando el listar especialidades

---
+++ src/Controller/EspecialidadController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Especialidad;
+use App\Form\EspecialidadType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/especialidad")
+ */
+class EspecialidadController extends AbstractController
+{
+    /**
+     * @Route("/", name="especialidad_index", methods={"GET"})
+     */
+    public function index(): Response
+    {
+        $especialidads = $this->getDoctrine()
+            ->getRepository(Especialidad::class)
+            ->findAll();
+
+        return $this->render('especialidad/index.html.twig', [
+            'especialidads' => $especialidads,
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="especialidad_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $especialidad = new Especialidad();
+        $form = $this->createForm(EspecialidadType::class, $especialidad);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($especialidad);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('especialidad_index');
+        }
+
+        return $this->render('especialidad/new.html.twig', [
+            'especialidad' => $especialidad,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="especialidad_show", methods={"GET"})
+     */
+    public function show(Especialidad $especialidad): Response
+    {
+        return $this->render('especialidad/show.html.twig', [
+            'especialidad' => $especialidad,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="especialidad_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Especialidad $especialidad): Response
+    {
+        $form = $this->createForm(EspecialidadType::class, $especialidad);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('especialidad_index');
+        }
+
+        return $this->render('especialidad/edit.html.twig', [
+            'especialidad' => $especialidad,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="especialidad_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Especialidad $especialidad): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$especialidad->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($especialidad);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('especialidad_index');
+    }
+}
---
+++ src/Form/EspecialidadType.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Especialidad;
+use App\EventListener\EspecialidadImageSubscriber;
+use App\Service\ImageUploader;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EspecialidadType extends AbstractType
+{
+    private $imageUploader;
+    private $entityManager;
+    private $targetDirectory;
+
+    public function __construct(ImageUploader $imageUploader, EntityManagerInterface $entityManager, $targetDirectory){
+        $this->imageUploader = $imageUploader;
+        $this->entityManager = $entityManager;
+        $this->targetDirectory = $targetDirectory;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nombre',TextType::class, array(
+                'label'=>'Nombre',
+                'attr' => array('class'=>'form-control')
+            ))
+            ->add('descripcion', TextareaType::class, array(
+                'label'=>'Descripción',
+                'attr' => array('class'=>'form-control')
+            ))
+        ;
+        $builder
+            ->add('imagen', HiddenType::class, array(
+                'data' => null
+            ))
+            ->addEventSubscriber(new EspecialidadImageSubscriber($this->imageUploader, $this->entityManager, $this->targetDirectory));
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Especialidad::class,
+        ]);
+    }
+}
